6365fe5aa9914087ebfc6c1470caecb1406f1d35
ze3roop <77748133+ze3roop@users.noreply.github.com>
2022-11-02 15:24:42 -0600

    adding .env, recipes.ejs, updated index.js, updated create.sql, and er diagram file to change on drawio if needed

---
+++ src/init_data/create.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) SERIAL PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS grocery_list CASCADE;
+CREATE TABLE grocery_list(
+    grocery_list_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS ingredients;
+CREATE TABLE ingredients(
+    ingredient_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS recipes;
+CREATE TABLE IF NOT EXISTS recipes(
+    recipe_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS users_to_grocery_lists;
+CREATE TABLE IF NOT EXISTS users_to_grocery_lists(
+    user_id SMALLINT,
+    grocery_list_id SMALLINT
+);
+
+DROP TABLE IF EXISTS grocery_list_to_ingredients;
+CREATE TABLE IF NOT EXISTS grocery_list_to_ingredients(
+    grocery_list_id SMALLINT,
+    ingredient_id SMALLINT
+);
+
+DROP TABLE IF EXISTS users_to_recipes;
+CREATE TABLE IF NOT EXISTS users_to_recipes(
+    user_id SMALLINT,
+    recipe_id SMALLINT
+);
+
+DROP TABLE IF EXISTS recipes_to_ingredients;
+CREATE TABLE IF NOT EXISTS recipes_to_ingredients(
+    recipe_id SMALLINT,
+    ingredient_id SMALLINT
+);
